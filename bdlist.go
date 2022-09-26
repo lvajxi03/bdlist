49450dd77cfcd418a0f7af1a6c105977df7b03f1
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2022-09-26 19:25:48 +0200

    Non-nil error returned when list or element are nil

--- bdlist.go
+++ bdlist.go
@@ -2,6 +2,10 @@
 // and its typical operations
 package bdlist
 
+import (
+	"errors"
+)
+
 // An Element contains value and pointers to the other Elements,
 // if any
 type Element struct {
@@ -29,163 +33,189 @@ func New() *BDList {
 	return list
 }
 
-// Append adds an element after Tail
-func (list *BDList) Append(element *Element) {
-	if element != nil {
-		if list.Length == 0 {
-			list.Head = element
-			list.Head.Prev = nil
-			list.Tail = element
-			list.Head.Next = list.Tail
-			list.Tail.Prev = list.Head
-			list.Tail.Next = nil
-			list.Length++
-		} else {
-			list.Tail.Next = element
-			element.Prev = list.Tail
-			element.Next = nil
-			list.Tail = list.Tail.Next
-			list.Length++
-		}
+// Append adds an element after Tail.
+// Returns non-nil error if element or list are nil.
+func (list *BDList) Append(element *Element) error {
+	if list == nil {
+		return errors.New("nil list")
 	}
+	if element == nil {
+		return errors.New("nil element")
+	}
+	if list.Length == 0 {
+		list.Head = element
+		list.Tail = element
+	} else {
+		list.Tail.Next = element
+		element.Prev = list.Tail
+		list.Tail = list.Tail.Next
+	}
+	list.Length++
+	return nil
 }
 
-// Prepend adds an element before Head
-func (list *BDList) Prepend(element *Element) {
-	if element != nil {
-		if list.Length == 0 {
-			list.Head = element
-			list.Head.Prev = nil
-			list.Tail = element
-			list.Head.Next = list.Tail
-			list.Tail.Prev = list.Head
-			list.Tail.Next = nil
-			list.Length++
-		} else {
-			list.Head.Prev = element
-			element.Next = list.Head
-			element.Prev = nil
-			list.Head = list.Head.Prev
-			list.Length++
-		}
+// Prepend adds an element before Head.
+// Returns non-nil error if element or list are nil.
+func (list *BDList) Prepend(element *Element) error {
+	if list == nil {
+		return errors.New("nil list")
 	}
+	if element == nil {
+		return errors.New("nil element")
+	}
+	if list.Length == 0 {
+		list.Head = element
+		list.Tail = element
+	} else {
+		list.Head.Prev = element
+		element.Next = list.Head
+		list.Head = list.Head.Prev
+	}
+	list.Length++
+	return nil
+
 }
 
 // AppendVal creates an Element with given value
-// and adds it after Tail
-func (list *BDList) AppendVal(value any) {
+// and adds it after Tail.
+// Returns non-nil error if element or list are nil.
+func (list *BDList) AppendVal(value any) error {
+	if list == nil {
+		return errors.New("nil list")
+	}
 	element := &Element{Value: value}
-	list.Append(element)
+	return list.Append(element)
 }
 
 // PrependVal creates an Element with given value
-// and addis it before Head
-func (list *BDList) PrependVal(value any) {
+// and adds it before Head.
+// Returns error if element or list are nil.
+func (list *BDList) PrependVal(value any) error {
+	if list == nil {
+		return errors.New("nil list")
+	}
 	element := &Element{Value: value}
-	list.Prepend(element)
+	return list.Prepend(element)
 }
 
 // InsertAt adds an element at the given position
 // (zero-based index)
 //
 // If pos is negative, InsertAt prepends the element
-// If pos is higher than list lenthg, InsertAt appends the element
-func (list *BDList) InsertAt(pos int, element *Element) {
-	if element != nil {
-		if pos >= list.Length {
-			list.Append(element)
-		} else if pos < 0 {
-			list.Prepend(element)
-		} else {
-			cur := 0
-			for iter := list.Head; iter != nil; iter = iter.Next {
-				if cur == pos {
-					element.Next = iter
-					element.Prev = iter.Prev
-					iter.Prev = element
-					element.Prev.Next = element
-				}
-				cur++
+// If pos is higher than list lenthg, InsertAt appends the element.
+// Returns non-nil error if element or list are nil.
+func (list *BDList) InsertAt(pos int, element *Element) error {
+	if list == nil {
+		return errors.New("nil list")
+	}
+	if element == nil {
+		return errors.New("nil element")
+	}
+	if pos >= list.Length {
+		list.Append(element)
+	} else if pos < 0 {
+		list.Prepend(element)
+	} else {
+		cur := 0
+		for iter := list.Head; iter != nil; iter = iter.Next {
+			if cur == pos {
+				element.Next = iter
+				element.Prev = iter.Prev
+				iter.Prev = element
+				element.Prev.Next = element
 			}
+			cur++
 		}
 	}
+	return nil
 }
 
 // InsertValAt creates an Element with given value
 // and adds it at given position (zero-based index)
 //
-//  If pos is negative, InsertAt prepends the element
-// If pos is higher than list lenthg, InsertAt appends the element
-func (list *BDList) InsertValAt(pos int, value any) {
+// If pos is negative, InsertAt prepends the element.
+// If pos is higher than list lenthg, InsertAt appends the element.
+// Returns non-nil error if list is nil.
+func (list *BDList) InsertValAt(pos int, value any) error {
+	if list == nil {
+		return errors.New("nil list")
+	}
 	element := NewElement(value)
-	list.InsertAt(pos, element)
+	return list.InsertAt(pos, element)
 }
 
-// Remove removes an Element and returns it.
-// If the element is not found, `nil` is returned
-func (list *BDList) Remove(element *Element) any {
-	if element != nil {
-		if element == list.Head {
-			if list.Head.Next != nil {
-				list.Head = list.Head.Next
-				list.Head.Prev = nil
-				list.Length--
-			} else {
-				list.Head = nil
-				list.Length--
-			}
-		} else if element == list.Tail {
-			list.Tail = list.Tail.Prev
-			list.Tail.Next = nil
-			list.Length--
-		} else {
-			element.Prev.Next = element.Next
-			element.Next.Prev = element.Prev
-			list.Length--
-		}
-		return element.Value
+// Remove removes an Element and returns its value.
+// If the element is not found, nil is returned.
+// Also, returns non-nil error if list or element are nil.
+func (list *BDList) Remove(element *Element) (any, error) {
+	if list == nil {
+		return nil, errors.New("nil list")
 	}
-	return nil
+	if element == nil {
+		return nil, errors.New("nil element")
+	}
+	if list.Head == nil {
+		return nil, errors.New("nil list")
+	}
+	if element == list.Head {
+		list.Head = list.Head.Next
+	} else if element == list.Tail {
+		list.Tail = list.Tail.Prev
+		list.Tail.Next = nil
+	} else {
+		element.Prev.Next = element.Next
+		element.Next.Prev = element.Prev
+	}
+	list.Length--
+	return element.Value, nil
 }
 
 // GetAt returns the element at given position
-// or `nil`, if not found
-func (list *BDList) GetAt(pos int) *Element {
+// or nil, if not found.
+// Also, returns non-nil error if list is nil.
+func (list *BDList) GetAt(pos int) (*Element, error) {
+	if list == nil {
+		return nil, errors.New("nil list")
+	}
 	var result *Element = nil
 	cur := 0
-	if list != nil {
-		for iter := list.Head; iter != nil; iter = iter.Next {
-			if cur == pos {
-				result = iter
-			}
-			cur++
+	for iter := list.Head; iter != nil; iter = iter.Next {
+		if cur == pos {
+			result = iter
 		}
+		cur++
 	}
-	return result
+	return result, nil
 }
 
 // GetValueAt returns value of the element at given
-// position or `nil`, if not found
-func (list *BDList) GetValueAt(pos int) any {
-	result := list.GetAt(pos)
-	if result != nil {
-		return result.Value
+// position or nil, if not found.
+// Also, returns non-nil error if list is nil.
+func (list *BDList) GetValueAt(pos int) (any, error) {
+	if list == nil {
+		return nil, errors.New("nil list")
 	}
-	return nil
+	result, err := list.GetAt(pos)
+	if result != nil {
+		return result.Value, err
+	}
+	return nil, err
 }
 
 // RemoveAt removes element at given position, if found,
 // and returns it.
-// `nil` is returned when element not found.
-func (list *BDList) RemoveAt(pos int) any {
-	cur := 0
-	if list != nil {
-		for iter := list.Head; iter != nil; iter = iter.Next {
-			if cur == pos {
-				return list.Remove(iter)
-			}
-			cur++
-		}
+// Returns nil when element not found.
+// Also, returns non-nil error if list is nil.
+func (list *BDList) RemoveAt(pos int) (any, error) {
+	if list == nil {
+		return nil, errors.New("nil list")
 	}
-	return nil
+	cur := 0
+	for iter := list.Head; iter != nil; iter = iter.Next {
+		if cur == pos {
+			return list.Remove(iter)
+		}
+		cur++
+	}
+	return nil, nil
 }
--- bdlist_test.go
+++ bdlist_test.go
@@ -12,29 +12,53 @@ func TestCreate(t *testing.T) {
 	}
 }
 
-func TestAppend(t *testing.T) {
+func TestAppend1(t *testing.T) {
 	list := bdlist.New()
 	list.AppendVal(1)
 	list.AppendVal(3)
 	list.AppendVal(5)
-	val := list.GetValueAt(1)
+	val, _ := list.GetValueAt(1)
 	if val != 3 {
 		t.Fatalf("Expected `3`, found `%d`", val)
 	}
 }
 
-func TestPrepend(t *testing.T) {
+func TestAppend2(t *testing.T) {
+	list := bdlist.New()
+	err := list.Append(nil)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+}
+
+func TestAppend3(t *testing.T) {
+	var list *bdlist.BDList = nil
+	err := list.AppendVal(5)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+}
+
+func TestPrepend1(t *testing.T) {
 	list := bdlist.New()
 	list.PrependVal(2)
 	list.PrependVal(4)
 	list.PrependVal(6)
-	val := list.GetValueAt(2)
+	val, _ := list.GetValueAt(2)
 	if val != 2 {
 		t.Fatalf("Expected `2`, found `%d`", val)
 	}
 }
 
-func TestRemove(t *testing.T) {
+func TestPrepend2(t *testing.T) {
+	list := bdlist.New()
+	err := list.Prepend(nil)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+}
+
+func TestRemove1(t *testing.T) {
 	list := bdlist.New()
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 21, 22, 2021, 2022}
 	for _, value := range arr {
@@ -44,7 +68,7 @@ func TestRemove(t *testing.T) {
 		t.Fatalf("Expected 16 elements in the list, found %d!", list.Length)
 	}
 	for iter := list.Head; iter != nil; iter = iter.Next {
-		if iter.Value.(int) %2 == 0 {
+		if iter.Value.(int)%2 == 0 {
 			list.Remove(iter)
 		}
 	}
@@ -52,8 +76,32 @@ func TestRemove(t *testing.T) {
 		t.Fatalf("Expected 8 elements in the list, found %d!", list.Length)
 	}
 
-	value := list.GetValueAt(2)
+	value, _ := list.GetValueAt(2)
 	if value != 11 {
 		t.Fatalf("Expected value: 11, found %d!", value)
+	}
+}
+
+func TestRemove2(t *testing.T) {
+	list := bdlist.New()
+	_, err := list.Remove(nil)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+}
+
+func TestRemove3(t *testing.T) {
+	var list *BDList = nil
+	err := list.RemoveAt(3)
+	if err == nil {
+		t.Fatal("Expected non-nil error")
+	}
+}
+
+func TestGetValueAt1(t *testing.T) {
+	list := bdlist.New()
+	val, _ := list.GetValueAt(0)
+	if val != nil {
+		t.Fatal("Expected nil value")
 	}
 }
