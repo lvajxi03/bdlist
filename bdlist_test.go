8fab583425c83531c075b1797b78df0de6f8cc25
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2022-09-26 15:57:53 +0200

    Added unit tests for Remove method

--- bdlist_test.go
+++ bdlist_test.go
@@ -33,3 +33,27 @@ func TestPrepend(t *testing.T) {
 		t.Fatalf("Expected `2`, found `%d`", val)
 	}
 }
+
+func TestRemove(t *testing.T) {
+	list := bdlist.New()
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 21, 22, 2021, 2022}
+	for _, value := range arr {
+		list.PrependVal(value)
+	}
+	if list.Length != 16 {
+		t.Fatalf("Expected 16 elements in the list, found %d!", list.Length)
+	}
+	for iter := list.Head; iter != nil; iter = iter.Next {
+		if iter.Value.(int) %2 == 0 {
+			list.Remove(iter)
+		}
+	}
+	if list.Length != 8 {
+		t.Fatalf("Expected 8 elements in the list, found %d!", list.Length)
+	}
+
+	value := list.GetValueAt(2)
+	if value != 11 {
+		t.Fatalf("Expected value: 11, found %d!", value)
+	}
+}
